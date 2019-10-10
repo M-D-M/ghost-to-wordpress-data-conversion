45612e2a08dd5f075461d71921906062e0192bef
M-D-M <m.d.mcnamara@gmail.com>
2019-10-09 21:46:16 -0400

    Initial commit.

---
+++ Ghost-to-Wordpress-Converter.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [string]$jsonFile
+)
+
+# $jsonFile = "C:\Downloads\ninalog.ghost.2019-10-07-22-42-26.json"
+
+function convertJSON() {
+    $json_data = Get-Content -Encoding UTF8 -Raw -Path $jsonFile | ConvertFrom-Json
+
+    $csv_file = "${jsonFile}.csv"
+    Remove-Item $csv_file -Force -ErrorAction SilentlyContinue
+
+    post_to_tag_mapping = $json_data.db[0].data.posts_tags
+    tag_mapping = $json_data.db[0].data.tags
+
+    ForEach($post in $json_data.db[0].data.posts) {
+        $post | Select-Object id,title,slug,html,published_at,custom_excerpt,page | Export-Csv $csv_file -Append -NoTypeInformation -Force -Encoding UTF8
+    }
+
+    write-host "Finished! Csv file created: ${csv_file}"
+}
+
+if ($jsonFile -ne "") {
+    convertJSON
+} 
+else {
+    write-host "Usage: <script name> <json file path>"
+}
